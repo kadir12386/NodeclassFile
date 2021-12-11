2954f09b8c6e05011760460bee0a6b3d1f5fc22a
kadir12386 <kadir12386@gmail.com>
2021-12-11 07:28:11 +0300

    1changes

--- routes/movies.js
+++ routes/movies.js
@@ -6,13 +6,14 @@ import {
   insertMovie,
   updateMovieById,
 } from "../helper.js";
-import { auth } from "./middleware/auth.js";
+// import { auth } from "./middleware/auth.js";
 
 const router = express.Router();
 
 router
   .route("/")
-  .get(auth, async (request, response) => {
+  // .get(auth, async (request, response) => {
+  .get(async (request, response) => {
     console.log("Query", request.query);
     const filter = request.query;
     const filterMovies = await getMovie(filter);
@@ -20,7 +21,8 @@ router
     response.send(filterMovies);
   })
   //=======================post method===================
-  .post(auth, async (request, response) => {
+  // .post(auth, async (request, response) => {
+  .post(async (request, response) => {
     const data = request.body;
     console.log("data", data);
     const result = await insertMovie(data);
@@ -30,7 +32,8 @@ router
 //=======================filter method===================
 router
   .route("/:id")
-  .get(auth, async (request, response) => {
+  // .get(auth, async (request, response) => {
+  .get(async (request, response) => {
     console.log(request.params);
     const { id } = request.params;
     const movie = await getMovieById(id);
@@ -39,7 +42,8 @@ router
     movie ? response.send(movie) : response.status(404).send(notFound);
   })
   //=======================Delete method===================
-  .delete(auth, async (request, response) => {
+  // .delete(auth, async (request, response) => {
+  .delete(async (request, response) => {
     console.log(request.params);
     const { id } = request.params;
     const movie = await deleteMovieById(id);
@@ -48,7 +52,8 @@ router
     movie ? response.send(movie) : response.status(404).send(notFound);
   })
   //=======================updated method =================== //similary to post
-  .put(auth, async (request, response) => {
+  // .put(auth, async (request, response) => {
+  .put(async (request, response) => {
     const { id } = request.params;
     const data = request.body;
     console.log("data", data);
